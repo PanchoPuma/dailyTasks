0d5ab8bb58d477a3f76dfa3f2de7c65fc7c3fad9
Francisco Puma <francis_pancho12@hotmail.com>
2021-05-15 13:05:11 -0700

    getting current time vs time in schedule

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 //today's date using moment
 var today = moment().format("dddd, MMMM Do");
 $("#currentDay").text(today);
+
+var currentTimeBrowser = moment().hour();
+console.log (currentTimeBrowser);
+
+$(".time-block").each(function () {
+    var scheduleHour = parseInt($(this).attr("id").split("hour")[0]);
+console.log (scheduleHour);
+});

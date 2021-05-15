fdb787dd937da9b72fa47c6e557f738034db1d88
Francisco Puma <francis_pancho12@hotmail.com>
2021-05-15 13:50:08 -0700

    set foundation for next steps

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,15 @@
 var today = moment().format("dddd, MMMM Do");
 $("#currentDay").text(today);
 
+// highlight text area based on the hour
 var hourHighlight = function () {
 
     var currentTimeBrowser = moment().hour();
-    console.log (currentTimeBrowser);
+    //console.log (currentTimeBrowser);
 
     $(".time-block").each(function () {
         var scheduleHour = parseInt($(this).attr("id").split("hour")[0]);
-    console.log (scheduleHour);
+    //console.log (scheduleHour);
 
     if (currentTimeBrowser === scheduleHour) {
         $(this).addClass("present");
@@ -20,6 +21,24 @@ var hourHighlight = function () {
     }
     });
 
-    console.log (currentTimeBrowser);
+    //console.log (currentTimeBrowser);
 };
+
 hourHighlight ();
+
+// saving text area
+
+var details = $("#eventDetails1").val();
+console.log (details);
+
+    // //var saveEventDetails = function() {
+    //     localStorage.setItem("EventDetails", JSON.stringify(details));
+    //   };
+    
+// load existing values 
+
+    // //var loadEventDetails = function() {
+        //     localStorage.getItem("EventDetails", JSON.stringify(details));
+        //   };
+        //loadEventDetails();
+
